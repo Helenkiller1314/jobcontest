63443cef2a7129057e01ef635792d7f048504065
Helen <helenmayanna@gmail.com>
2017-04-21 17:21:05 +0800

    admin can see resume list

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,6 @@
 class Job < ApplicationRecord
-  has_many :resumess
-  
+  has_many :resumes
+
   validates :title, presence: true
   validates :wage_upper_bound, presence: true
 validates :wage_lower_bound, presence: true
